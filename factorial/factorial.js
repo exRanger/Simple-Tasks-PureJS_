a3a1d6b7bea4389fc009e62d1c8e7f08b77efdf6
Ilgiz <lmnasxzz@gmail.com>
2021-05-31 20:35:08 +0300

    Update factorial.js

--- factorial/factorial.js
+++ factorial/factorial.js
@@ -1,4 +1,4 @@
-const factorial = (n) => (n != u) ? n * factorial(n - 1) : 1;
+const factorial = (n) => (n != 1) ? n * factorial(n - 1) : 1;
 
 console.log( factorial(0) ); // 1
 console.log( factorial(1) ); // 1
