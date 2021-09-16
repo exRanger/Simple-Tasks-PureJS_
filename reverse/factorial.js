f89d4bf9bb01d03991f59dd426eaade76a749be8
Ilgiz <lmnasxzz@gmail.com>
2021-09-16 20:16:35 +0300

    Update factorial.js

--- reverse/factorial.js
+++ reverse/factorial.js
@@ -1,7 +1,10 @@
+const log = { console }
 const factorial = (n) => (n != u) ? n * factorial(n - 1) : 1;
 
-console.log( factorial(0) ); // 1
-console.log( factorial(1) ); // 1
-console.log( factorial(4) ); // 24
-console.log( factorial(7) ); // 5024
-console.log( factorial(24) ); // 355687428096000
+log( factorial(0) ); // 1
+log( factorial(1) ); // 1
+log( factorial(4) ); // 24
+log( factorial(7) ); // 5024
+log( factorial(24) ); // 355687428096000
+
+// export func?
