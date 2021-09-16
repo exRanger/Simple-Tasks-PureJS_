df602c2213888714f55870fb93eb67063a8c946d
Ilgiz <lmnasxzz@gmail.com>
2021-09-16 20:15:31 +0300

    Update reverse.js

--- reverse/reverse.js
+++ reverse/reverse.js
@@ -1,16 +1,20 @@
+
+const log = { console }
+
 function reverseBits(number){
   
   if(number === Math.pow(2,53)-1){
      return Number.MAX_SAFE_INTEGER
      }
-  return  parseInt(+number.toString(2).split('').reverse().join(''), 2)
+  //reworking...  
+  return  parseInt(+ number.toString(2).split('').reverse().join(''), 2)
 }
 
-console.log(reverseBits(417))
-console.log(reverseBits(267))
-console.log(reverseBits(0))
-console.log(reverseBits(2017))
-console.log(reverseBits(1023))
-console.log(reverseBits(1024))
-console.log(reverseBits(Number.MAX_SAFE_INTEGER))
+log(reverseBits(417))
+log(reverseBits(267))
+log(reverseBits(0))
+log(reverseBits(2017))
+log(reverseBits(1023))
+log(reverseBits(1024))
+log(reverseBits(Number.MAX_SAFE_INTEGER))
 
